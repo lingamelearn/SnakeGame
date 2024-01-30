54a238b61016df8d0d83fb2fde9fabe9429586ce
lingamelearn <hemanth.elearn@gmail.com>
2024-01-30 16:54:22 +0530

    source folder

---
+++ src/snakegame.c
@@ -0,0 +1,16 @@
+/*
+ * snakegame.c
+ *
+ *  Created on: Jan 30, 2024
+ *      Author: Lingam
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <NCURSES/ncurses.h>
+
+int main(void) {
+	return 0;
+}
